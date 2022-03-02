22078409eef259e5a74db16202145dd4e7b35540
sikako <sikako.setp08@nctu.edu.tw>
2022-03-02 08:54:53 -0800

    advice lock

---
+++ flock.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/file.h>
+
+
+int main(int argc, char* argv[]){
+	int fd;
+	int ret;
+	char opt;
+	fd = open(argv[1], O_WRONLY);
+	printf("fd = %d is opened\n", fd);
+
+	sscanf(argv[2], "%c", &opt);
+	switch(opt){
+		case 's':
+			ret = flock(fd, LOCK_SH);
+			break;
+		case 'e':
+			ret = flock(fd, LOCK_EX);
+			break;
+		case 'u':
+			ret = flock(fd, LOCK_UN);
+			break;
+		default:
+			printf("input error\n");
+	}
+	if(ret != 0) perror("flock");
+	printf("end\n");
+	getchar();
+	return 0;
+}
